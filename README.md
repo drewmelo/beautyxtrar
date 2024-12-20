60b2ed0f237bb193580eb38e6c1498ec63886222
André <andremelopix@hotmail.com>
2024-12-20 13:13:00 -0300

    some improvements

---
+++ README.md
@@ -0,0 +1,6 @@
+
+<!-- README.md é gerado pelo README.Rmd. -->
+
+# beautyxtrar <img src="man/figuras/logo.png" align="right" alt="" width="160px" />
+
+[![lifecycle](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://www.tidyverse.org/lifecycle/#experimental)
--- tests/testthat/test-beautyxtrar.R
+++ tests/testthat/test-beautyxtrar.R
@@ -6,3 +6,15 @@ ggplot(cars,
        aes(speed, dist)) +
   geom_point() +
   theme_xtra()
+
+ggplot(iris, aes(Sepal.Length, Sepal.Width, col = Species)) +
+  geom_point() +
+  dark_theme_xtra()
+
+ggplot(iris, aes(Sepal.Length, Sepal.Width, col = Species)) +
+  geom_point() +
+  theme_xtra()
+
+ggplot(iris, aes(Sepal.Length, Sepal.Width, col = Species)) +
+  geom_point() +
+  theme_academic()
